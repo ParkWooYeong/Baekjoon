82f49234edbc480e8aa25a971b199704e0905199
ParkWooYeong <uyeongi@naver.com>
2026-03-09 19:13:56 +0900

    1010 백준

--- 1010.js
+++ 1010.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const T = parseInt(input[0]);
-
-// DP를 이용한 조합 테이블 만들기 (최대 30x30)
 const memo = Array.from(Array(30), () => Array(30).fill(0));
 
 for (let i = 1; i < 30; i++) {
@@ -19,7 +17,6 @@ for (let i = 1; i < 30; i++) {
 const results = [];
 for (let i = 1; i <= T; i++) {
     const [N, M] = input[i].split(' ').map(Number);
-    // M개 중에서 N개를 고르는 조합 (M C N)
     results.push(memo[M][N]);
 }
 

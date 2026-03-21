e29ddc792e86608994523be8d10be5c62ef76e3d
ParkWooYeong <uyeongi@naver.com>
2026-03-21 20:22:42 +0900

    editdit

--- 1181.js
+++ 1181.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = fs.readFileSync(0,"utf8").toString().trim().split('\n');
 const N = input[0];
 const words = input.slice(1);
 
